295f193bfe939a735918f2b231febfaa7a5da7dd
Kaushal Gore <kaushalgore24@gmail.com>
2025-05-11 23:58:46 +0530

    need changes

---
+++ P011.py
@@ -0,0 +1,8 @@
+#  10. Prime number within a given range
+num=int(input("Enter a number: "))
+for i in range (2,num+1):
+    if num%i==0:
+        print(num,"is Not A Prime Number")
+        break
+else:
+    print(num,"is A Prime Number")
